b0f4fa196609a6c2b5bf8cb3e94404d60cd42ab5
Nicolas Conte <contenicolascv@gmail.com>
2021-06-22 20:49:48 -0300

    v.1

--- open_pdf/OpenPDF.py
+++
@@ -1,9 +0,0 @@
-from open_pdf.src.image import Image
-from open_pdf.src.pdf import PDF
-
-class OpenPDF(Image, PDF):
-    def __init__(self, output_dir) -> None:
-        self.image = Image(output_dir)
-        self.pdf = PDF(output_dir)
-
-    
--- open_pdf/image.py
+++ open_pdf/image.py
@@ -1,14 +1,15 @@
 from PIL import Image as PillowImage
 
-from .storage import Storage
+from .file import File
 
-class Image(Storage):
+class Image(File):
     def __init__(self, output_dir):
-        Storage.__init__(self, output_dir)
+        File.__init__(self)
         self.__current_images: list = []
+        self.__output_dir = output_dir
 
     def __save(self, images:list, name:str) -> bool:
-        return images[0].save(f"{self._output_dir}/{name}", save_all=True, append_images=images[1:len(images)])    
+        return images[0].save(f"{self.__output_dir}/{name}", save_all=True, append_images=images[1:len(images)])    
 
     def convert_to_pdf(self, images: list, name: str="Convertido_por_OpenPDF.pdf") -> bool:
 
@@ -18,12 +19,12 @@ class Image(Storage):
             for image in images:
                 self.__current_images.append(PillowImage.open(image).convert("RGB"))
 
-            if self._exists(self._filename):
+            if self._exists(self.__output_dir, self._filename):
                 self._filename = self._rename_file(self._filename) 
 
             self.__save(self.__current_images, f"{self._filename}")
 
-            return self._exists(self._filename)
+            return self._exists(self.__output_dir, self._filename)
 
         except Exception as e:
             print(f"Cannot convert image -> {str(e)}")
